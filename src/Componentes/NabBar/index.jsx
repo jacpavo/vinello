1af89518395e2e2b37e04f0c5c3aa2a2c26d1727
Javier Cascante <javier.cascante@me.com>
2022-01-12 05:17:04 -0300

    correccion nav

---
+++ src/Componentes/NabBar/index.jsx
@@ -0,0 +1,15 @@
+import './stylenav.css'
+
+const NavBar = () => {
+    return <nav> 
+                <ul>
+                    <li>Inicio</li>
+                    <li>Vinos</li>
+                    <li>Cervezas</li>
+                    <li>Espirituosas</li>
+                    <li>Contacto</li>
+                </ul>
+         </nav>;
+};
+
+export default NavBar;
