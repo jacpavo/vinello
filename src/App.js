34351db549915c5eafac9c8bede881bf1aa33a2d
Javier Cascante <javier.cascante@me.com>
2022-01-15 17:36:31 -0300

    linkedpages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Componentes/NabBar';
 
+
 function App() {
   return (
     <div className="App">
--- src/Componentes/NabBar/index.jsx
+++ src/Componentes/NabBar/index.jsx
@@ -2,12 +2,23 @@ import './stylenav.css'
 
 const NavBar = () => {
     return <nav> 
+                <img src="./Vinelo-Logo.svg" alt="logo" class="logo"></img>
                 <ul>
-                    <li>Inicio</li>
-                    <li>Vinos</li>
-                    <li>Cervezas</li>
-                    <li>Espirituosas</li>
-                    <li>Contacto</li>
+                    <li>
+                        <a href="..//Paginas/Inicio.js">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="..//Paginas/Vinos.js">Vinos</a>
+                    </li>
+                    <li>
+                        <a href="..//Paginas/Cervezas.js">Cervezas</a>
+                    </li>
+                    <li>
+                        <a href="..//Paginas/Espirituosas.js">Espirituosas</a>
+                    </li>
+                    <li>
+                        <a href="..//Paginas/Contacto.js">Contacto</a>
+                    </li>
                 </ul>
          </nav>;
 };
